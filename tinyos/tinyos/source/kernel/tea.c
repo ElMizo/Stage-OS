b90d433670bb2c0029f9ff1e3a5aee8f28d3ca8a
Chattaoui7 <165184547+Chattaoui7@users.noreply.github.com>
2024-07-24 18:51:52 +0100

    Update tea.c

--- tinyos/tinyos/source/kernel/tea.c
+++ tinyos/tinyos/source/kernel/tea.c
@@ -10,50 +10,50 @@
 #include "../internal.h"
 
 
-#define MAGIC 0x9E3779B9
-#define MIX   (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p%k)^e] ^ z)))
+#define MAGIC 0x9E3779B9  // Constant used in the TEA algorithm
+#define MIX   (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p%k)^e] ^ z)))  // Mix function used for encryption
 
 int tea_encrypt(char *text, char *key) {
-	unsigned int y, p, e;
+    unsigned int y, p, e;
 
-	unsigned int n = kstrlen(text);
-	unsigned int k = kstrlen(key);
-	unsigned int rounds = 6 + 64/k + 64/n;
-	unsigned int sum = 0;
-	unsigned int z = text[n-1];
+    unsigned int n = kstrlen(text);  // Length of the text to be encrypted
+    unsigned int k = kstrlen(key);   // Length of the encryption key
+    unsigned int rounds = 6 + 64/k + 64/n;  // Number of encryption rounds
+    unsigned int sum = 0;  // Sum initialized to 0
+    unsigned int z = text[n-1];  // Initialize z to the last character of the text
 
-	do {
-		sum += MAGIC;
-		e = (sum >> 2) & 3;
-		for (p = 0; p < n-1; p++) {
-			y = text[p + 1];
-			z = text[p] += MIX;
-		}
-		y = text[0];
-		z = text[n - 1] += MIX;
-	} while (--rounds);
-	return 1;
+    do {
+        sum += MAGIC;  // Increment sum by MAGIC constant
+        e = (sum >> 2) & 3;  // Calculate e for the MIX function
+        for (p = 0; p < n-1; p++) {
+            y = text[p + 1];  // Set y to the next character in the text
+            z = text[p] += MIX;  // Update the current character using the MIX function
+        }
+        y = text[0];  // Set y to the first character in the text
+        z = text[n - 1] += MIX;  // Update the last character using the MIX function
+    } while (--rounds);  // Repeat for the number of rounds
+    return 1;  // Return 1 to indicate successful encryption
 }
 
 
 int tea_decrypt(char *text, char *key) {
-	unsigned int z, p, e;
+    unsigned int z, p, e;
 
-	unsigned int n = kstrlen(text);
-	unsigned int k = kstrlen(key);
-	unsigned int rounds = 6 + 64/k + 64/n;
-	unsigned int sum = rounds*MAGIC;
-	unsigned int y = text[0];
+    unsigned int n = kstrlen(text);  // Length of the text to be decrypted
+    unsigned int k = kstrlen(key);   // Length of the decryption key
+    unsigned int rounds = 6 + 64/k + 64/n;  // Number of decryption rounds
+    unsigned int sum = rounds*MAGIC;  // Initialize sum to rounds * MAGIC constant
+    unsigned int y = text[0];  // Initialize y to the first character of the text
 
-	do {
-		e = (sum >> 2) & 3;
-		for (p = n - 1; p > 0; p--) {
-			z = text[p - 1];
-			y = text[p] -= MIX;
-		}
-		z = text[n - 1];
-		y = text[0] -= MIX;
-	} while ((sum -= MAGIC) != 0);
+    do {
+        e = (sum >> 2) & 3;  // Calculate e for the MIX function
+        for (p = n - 1; p > 0; p--) {
+            z = text[p - 1];  // Set z to the previous character in the text
+            y = text[p] -= MIX;  // Update the current character using the MIX function
+        }
+        z = text[n - 1];  // Set z to the last character in the text
+        y = text[0] -= MIX;  // Update the first character using the MIX function
+    } while ((sum -= MAGIC) != 0);  // Repeat for the number of rounds
 
-	return 1;
+    return 1;  // Return 1 to indicate successful decryption
 }
