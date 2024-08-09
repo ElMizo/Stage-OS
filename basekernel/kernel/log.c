fdaaf82b811d0ea8ef29da244d63ceb075842340
Hamza Aarab <elmizo@Ubuntu.myguest.virtualbox.org>
2024-08-09 22:08:41 +0100

    Revert "dos2unix"
    This reverts commit 3a550d052cdfe8e870953cd48e9f77d414c615df.

--- basekernel/kernel/log.c
+++ basekernel/kernel/log.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "log.h"
 #include "kernel/types.h"
 #include "string.h"
@@ -25,3 +26,31 @@ void printk(int level, const char *fmt, ...) {
 void log_error(const char *message) {
     printf(LOG_LEVEL_ERR, "%s", message);
 }
+=======
+#include "log.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+#define LOG_LEVEL_ERR 3
+
+void printk(int level, const char *fmt, ...) {
+    va_list args;
+    char buffer[1024];
+    const char *level_str;
+
+    switch (level) {
+        case LOG_LEVEL_ERR: level_str = "ERR"; break;
+        default:            level_str = "UNKNOWN"; break;
+    }
+
+    va_start(args, fmt);
+    vsprintf(buffer, fmt, args);
+    va_end(args);
+
+    printf("[%s] %s\n", level_str, buffer);
+}
+
+void log_error(const char *message) {
+    printk(LOG_LEVEL_ERR, "%s", message);
+}
+>>>>>>> parent of 3a550d0 (dos2unix)
