6bcb21cb7b04392e4579e60d959919c990cac04e
Kh4lIdEk <koussamikhalid0@gmail.com>
2024-08-11 19:33:05 +0100

    refresh_branch

--- MemoryC/Header.h
+++
@@ -1,7 +0,0 @@
-#ifndef MEMORY_H
-#define MEMORY_H
-
-void* malloc(size_t size);
-void free(void* ptr);
-
-#endif
--- basekernel/kernel/font.h
+++
@@ -1,9 +0,0 @@
-#ifndef FONT_H
-#define FONT_H
-
-extern unsigned char fontdata[];
-
-#define FONT_WIDTH 8
-#define FONT_HEIGHT 8
-
-#endif
